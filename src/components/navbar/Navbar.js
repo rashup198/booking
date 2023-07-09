d2114227815e96628c3f3272b2d7865fc2d86ac3
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-07-09 13:31:24 +0530

    updated Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import "./navbar.css"
+import { Link } from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
+  const { loginWithRedirect } = useAuth0();
+  const { logout } = useAuth0();
+  const {user,isAuthenticated} = useAuth0();
+
   return (
     <div className='navbar'>
     <div className='navContainer'>
-        <span className='logo'>
+      <Link to='/' className='navLogo'>
+      <span className='logo'>
             PandeyBooking
         </span>
+      </Link>
+    
+        
         <div className='navItems'>
-            <button className='navButton'>Register</button>
-            <button className='navButton'>Login</button>
+
+       { isAuthenticated && <div className='user'>
+          <img className='user_img' src={user.picture} alt={user.name} />
+          <p className='user_detail'>{user.name}</p>
+        </div>
+       }
+       
+       
+       <div>
+        {
+          
+          isAuthenticated ?<button className='btnLogin' onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</button> :
+          <button className='btnLogin' onClick={() => loginWithRedirect()}>Log In</button>
+          }
+          </div>
+
         </div>
     </div>
       
