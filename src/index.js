fc76a374e5d1067d5ed6d976a669ce378b1a8c53
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-07-09 13:30:53 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { Auth0Provider } from '@auth0/auth0-react';
+
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
+  <Auth0Provider
+    domain="dev-8lc77ppfclgdb2xj.us.auth0.com"
+    clientId="KwpuDxNIRbA08nKnko3XH6DZJrwwV6sp"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>,
 );
